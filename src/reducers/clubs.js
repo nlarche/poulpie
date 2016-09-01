6e3f765b18a0d2c8afba57e26687cfbae58c61eb
nilar <nicolas.larche@rca.fr>
2016-09-01 12:15:16 +0200

    add vscode settings.json

--- src/reducers/clubs.js
+++ src/reducers/clubs.js
@@ -1,13 +1,13 @@
 export default function clubs(state = [], action) {
   switch (action.type) {
     case 'ADD_CLUB':
-    console.log(action.club);
       return [...state, {
         ...action.club
-      }];
+      }
+      ];
     default:
   return state;
-  }
+}
 }
 
 export function selectedClub(state = null, action) {
